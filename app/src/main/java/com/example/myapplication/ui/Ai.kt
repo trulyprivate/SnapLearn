a6d1341c96464468612e6b9eece67a98afde04c6
Deeghayu Adhikari <63475473+DeegayuA@users.noreply.github.com>
2024-04-23 23:09:34 +0530

    ane manda okkoma case

--- app/src/main/java/com/example/myapplication/ui/Ai.kt
+++ app/src/main/java/com/example/myapplication/ui/Ai.kt
@@ -3,11 +3,7 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -19,22 +15,18 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.myapplication.ui.MainScreen
-import com.google.ai.client.generativeai.GenerativeModel
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.google.ai.client.generativeai.GenerativeModel
 
 class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,12 +41,11 @@ class SecondActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     val generativeModel = GenerativeModel(
-                            modelName = "gemini-pro",
-                            apiKey = apiKey
+                        modelName = "gemini-pro",
+                        apiKey = apiKey
                     )
                     val viewModel = AnswerViewModel(generativeModel)
                     AnswerRoute(detectedText, viewModel)
-//                    Text(text = "API Key: $apiKey")
                 }
             }
         }
@@ -85,30 +76,31 @@ fun AnswerScreen(
             .padding(all = 8.dp)
             .verticalScroll(rememberScrollState())
     ) {
-        Row {
-            TextField(
-                value = prompt,
-                label = { Text(stringResource(R.string.summarize_label)) },
-                placeholder = { Text(stringResource(R.string.summarize_hint)) },
-                onValueChange = { prompt = it },
-                modifier = Modifier
-                    .weight(8f)
-            )
-            TextButton(
-                onClick = {
-                    if (prompt.isNotBlank()) {
-                        onAnswerClicked(prompt)
-                    }
-                },
+        val buttonHeight = 64.dp // Define the height of the button
+        TextField(
+            value = prompt,
+            label = { Text(stringResource(R.string.summarize_label)) },
+            placeholder = { Text(stringResource(R.string.summarize_hint)) },
+            onValueChange = { prompt = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(min = buttonHeight)
+        )
 
-                modifier = Modifier
-                    .weight(2f)
-                    .padding(all = 4.dp)
-                    .align(Alignment.CenterVertically)
-            ) {
-                Text(stringResource(R.string.action_go))
-            }
+        TextButton(
+            onClick = {
+                if (prompt.isNotBlank()) {
+                    onAnswerClicked(prompt)
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            contentPadding = PaddingValues(vertical = 12.dp)
+        ) {
+            Text(stringResource(R.string.action_go))
         }
+
         when (uiState) {
             AnswerUiState.Initial -> {
                 // Nothing is shown
